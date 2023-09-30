14f0c64133f2032b47e6e062c8175044b3bf90ae
Mufrat Karim Aritra <mufratkarimaritra@gmail.com>
2023-09-30 00:42:08 -0400

    Dimmed buttons after press for 0.2s

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,13 +18,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound(title: sender.currentTitle!)
+        playSound(button: sender)
     }
     
-    func playSound(title: String) {
-        let url = Bundle.main.url(forResource: title, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
+    func playSound(button: UIButton) {
+        let url = Bundle.main.url(forResource: button.currentTitle!, withExtension: "wav")
+        button.layer.opacity = Float.random(in: 0.1...0.5)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.player = try! AVAudioPlayer(contentsOf: url!)
+            self.player.play()
+            button.layer.opacity = 1
+        }
+        
+
+
     }
     
     
